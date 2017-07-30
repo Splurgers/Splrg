75a9428474b4b9e0c5121c0e5034f7d42d1c08ef
Arash <arash@Arashs-MacBook-Pro.local>
2017-07-30 09:14:15 -0700

    added update/edit/delete splurges

---
+++ src/services/splurge_actions.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+
+import { ModalController } from 'ionic-angular';
+
+import { FormPage } from '../pages/form/form';
+
+import { SPLURGE } from '../models/splurge.model';
+
+@Injectable()
+export class SplurgeActionsService {
+
+  constructor(public modalCtrl: ModalController) {}
+
+  goToSplurgeForm(splurgeToEdit? : SPLURGE) {
+    console.log('SPLURGE', splurgeToEdit);
+    let modal = this.modalCtrl.create(FormPage, splurgeToEdit);
+    modal.present();
+  }
+}
