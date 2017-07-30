351847b71bd721cb51bf9ae0d954e155af4d8247
Brendan Dugan <brendandugan@MacBook-Pro-3.local>
2017-07-30 12:38:52 -0700

    [UI] Add toast for splurge use

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -123,5 +123,11 @@ export class ListPage {
 
     let selectedSplurge = currentSplurges.find((s) => s.id === splurge.id);
     this.splurgeService.addUseDateToSplurge(selectedSplurge);
+    let toast = this.toastCtrl.create({
+      message: `You Splurged with "${selectedSplurge.description}". Enjoy!`,
+      duration: 3000,
+      position: 'top'
+    });
+    toast.present();
   }
 }
