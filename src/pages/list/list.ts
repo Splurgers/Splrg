309d6b7d0eb5fab96eb30abadc8eb865556bb2ea
Arash <arash@Arashs-MacBook-Pro.local>
2017-08-16 20:43:56 -0700

    minor clean up, added logout feature, some splurge list logic, and camera config

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -3,80 +3,60 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ToastController } from 'ionic-angular';
 import { ActionSheetController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 import moment from 'moment'
 
 import { Observable } from "rxjs/Observable";
 import { SplurgeService } from '../../services/splurges.service';
 import { SplurgeActionsService } from '../../services/splurge_actions.service';
+import { UserService } from '../../services/user.service';
 import { SPLURGE } from '../../models/splurge.model';
+import { LoginPage } from '../login/login';
 
 
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html',
-  providers: [SplurgeService, SplurgeActionsService]
+  providers: [SplurgeService, SplurgeActionsService, UserService]
 })
 export class ListPage {
   SPLURGES: Observable<any>;
-
+  CameraOptions: CameraOptions;
 
   constructor(public navCtrl: NavController,
               public splurgeService: SplurgeService,
               public splurgeActionsService: SplurgeActionsService,
+              public userService: UserService,
               public toastCtrl: ToastController,
-              public actionSheetCtrl: ActionSheetController) {
+              public actionSheetCtrl: ActionSheetController,
+              private camera: Camera) {
 
-      // TODO: Understand observables so code below actually works
-      //  this.SPLURGES = splurgeService.SPLURGES.map((arraySplurg) => arraySplurg.map((splurg) => this.decorateSplurge(splurg)));
-       this.SPLURGES = splurgeService.SPLURGES
-       splurgeService.get();
+    this.CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.SPLURGES = splurgeService.SPLURGES;
+    splurgeService.get();
   }
 
   addSplurge() {
     this.splurgeActionsService.goToSplurgeForm();
   }
 
-  // TODO: Make this work properly with the this.SPLURGES...for now just use helper methods in view
-  // decorateSplurge(splurge) {
-  //   let done_count = 0;
-  //   let createdAt = moment(splurge.created_at);
-  //   let currentTime = moment();
+  takePicture() {
+    this.camera.getPicture(this.CameraOptions).then((imageData) => {
+        // imageData is either a base64 encoded string or a file URI
+        // If it's base64:
+        let base64Image = 'data:image/jpeg;base64,' + imageData;
+        console.log(base64Image);
+      }, (err) => {
+        // Handle error
+      });
 
-  //   let diff = currentTime.diff(createdAt, splurge.period.toLowerCase(), true);
-  //   // TODO: Check if this Math.floor is necessary
-  //   let startOfLatestInterval = createdAt.add(Math.floor(diff), splurge.period.toLowerCase());
-
-  //   for (let i = 0; i < splurge.uses_per_period; i++) {
-  //     let use = splurge.use_dates[splurge.use_dates.length - (i + 1)];
-  //     if (use && typeof use === 'object' && use.hasOwnProperty('date')) {
-  //       if (moment(use.date).isBetween(startOfLatestInterval, currentTime)) done_count++
-  //     }
-  //   }
-
-  //   return Object.assign(splurge, { splurges_left: splurge.uses_per_period - done_count})
-  // }
-
-   getSplurgesLeft(splurge) {
-    if (splurge && typeof splurge === 'object' && splurge.hasOwnProperty('created_at')) {
-      let done_count = 0;
-      let createdAt = moment(splurge.created_at);
-      let currentTime = moment();
-
-      let diff = currentTime.diff(createdAt, splurge.period.toLowerCase(), true);
-      // TODO: Check if this Math.floor is necessary
-      let startOfLatestInterval = createdAt.add(Math.floor(diff), splurge.period.toLowerCase());
-
-      for (let i = 0; i < splurge.uses_per_period; i++) {
-        let use = splurge.use_dates[splurge.use_dates.length - (i + 1)];
-        if (use && typeof use === 'object' && use.hasOwnProperty('date')) {
-          if (moment(use.date).isBetween(startOfLatestInterval, currentTime)) done_count++
-        }
-      }
-      return splurge.uses_per_period - done_count;
-    } else {
-      return 0;
-    }
   }
 
   showSplurgeCardActions(splurge: SPLURGE) {
@@ -106,28 +86,23 @@ export class ListPage {
     actionSheet.present();
   }
 
-  useSplurge(event, splurge) {
-    event.stopPropagation();
+  useSplurge({ $event, splurge, SPLURGES_LEFT }) {
+    $event.stopPropagation();
 
     // TODO: Add disabled class to naughty tickers that have a zero value
-    // if (splurge.splurges_left === 0) return;
-    if (this.getSplurgesLeft(splurge) === 0) return;
+    if (SPLURGES_LEFT === 0) return;
 
-    let currentSplurges = [];
-    let currentTime = moment().format();
-
-    this.SPLURGES.subscribe(
-      splurges => currentSplurges = splurges,
-      error => console.error(error.json())
-    );
-
-    let selectedSplurge = currentSplurges.find((s) => s.id === splurge.id);
-    this.splurgeService.addUseDateToSplurge(selectedSplurge);
+    this.splurgeService.addUseDateToSplurge(splurge);
     let toast = this.toastCtrl.create({
-      message: `You Splurged with "${selectedSplurge.description}". Enjoy!`,
+      message: `You Splurged with "${splurge.description}". Enjoy!`,
       duration: 3000,
       position: 'top'
     });
     toast.present();
+  }
+
+  logout() {
+    this.userService.logout();
+    this.navCtrl.push(LoginPage);
   }
 }
